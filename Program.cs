51a784ee41f06bb309875f54cb1c105eef02004d
Dell-User <Dell-User@DESKTOP-4LNHG9G>
2023-10-09 16:56:47 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._1_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //calling a method
+            SayHi();
+            // method with parameters
+            SayHiUser("Matimba");
+
+            // method with many parameters
+            SayHiAge("John", 45);
+            //freezing the console
+            Console.ReadLine();
+
+        }
+        // Creating a Method
+        static void SayHi()
+
+        {
+            Console.WriteLine("Hi");
+        }
+        // Method with one parameter
+        static void SayHiUser(string name)
+        {
+            Console.WriteLine("Hello" + name);
+        }
+        // Method with many parameters
+        static void SayHiAge(string name, int age)
+        {
+            Console.WriteLine("Hello" + name + "You are" + age);
+
+        }
+    }
+
+}
+
